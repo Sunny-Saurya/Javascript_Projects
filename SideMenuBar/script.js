bef9f176ab5fb73e622dcd11636bceae91ab3f20
Sunny_Saurya <sunnysaurya9955@gmail.com>
2024-07-20 20:54:39 +0530

    side menu bar

---
+++ SideMenuBar/script.js
@@ -0,0 +1,10 @@
+const header = document.querySelector("header");
+const hamburgerBtn = document.querySelector("#hamburger-btn");
+const closeMenuBtn = document.querySelector("#close-menu-btn");
+
+// Toggle mobile menu on hamburger button click
+hamburgerBtn.addEventListener("click", () => header.classList.toggle("show-mobile-menu"));
+
+// Close mobile menu on close button click
+closeMenuBtn.addEventListener("click", () => hamburgerBtn.click());
+
